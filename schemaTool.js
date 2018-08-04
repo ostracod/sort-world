a13ca60d6e786a9dd48c5d04fad6f870efebb00d
John Eisenmann <esperantanaso@gmail.com>
2018-08-04 16:19:24 -0400

    More database stuff

--- schemaTool.js
+++ schemaTool.js
@@ -6,6 +6,12 @@ var confirm = require("confirm-cli");
 
 var connection = null;
 
+function reportSqlError(error) {
+    console.log("Could not connect to MySQL.");
+    console.log(error.code);
+    console.log(error.sqlMessage);
+}
+
 function exitCleanly(exitValue) {
     if (typeof exitValue === "undefined") {
         exitValue = 0;
@@ -19,17 +25,164 @@ function exitCleanly(exitValue) {
 function printUsageAndExit() {
     console.log("Usage:");
     console.log("node schemaTool.js setUp");
+    console.log("node schemaTool.js verify");
     console.log("node schemaTool.js destroy");
     exitCleanly(1);
 }
 
-function setUpSchemaCommand() {
-    // TODO: Implement.
-    
-    exitCleanly();
+function databaseExists(done) {
+    connection.query(
+        "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?",
+        [databaseName],
+        function (error, results, fields) {
+            if (error) {
+                reportSqlError(error);
+                exitCleanly();
+                return;
+            }
+            done(results.length > 0);
+        }
+    );
 }
 
-function destroySchema() {
+function tableExists(table, done) {
+    connection.query(
+        "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?",
+        [databaseName, table.name],
+        function (error, results, fields) {
+            if (error) {
+                reportSqlError(error);
+                exitCleanly();
+                return;
+            }
+            done(results.length > 0);
+        }
+    );
+}
+
+function tableFieldExists(table, field, done) {
+    // TODO: Implement.
+    
+    done();
+}
+
+function setUpTableField(table, field, done) {
+    // TODO: Implement.
+    
+    done();
+}
+
+function setUpTableFields(table, done) {
+    // TODO: Implement.
+    
+    done();
+}
+
+function setUpTable(table, done) {
+    // TODO: Implement.
+    
+    done();
+}
+
+function setUpTables(done) {
+    // TODO: Implement.
+    
+    done();
+}
+
+function setUpDatabase(done) {
+    databaseExists(function(exists) {
+        if (exists) {
+            console.log("Database \"" + databaseName + "\" already exists.");
+            setUpTables(done);
+            return;
+        }
+        console.log("Creating database \"" + databaseName + "\"...");
+        connection.query(
+            "CREATE DATABASE " + databaseName,
+            [],
+            function (error, results, fields) {
+                if (error) {
+                    reportSqlError(error);
+                    exitCleanly();
+                    return;
+                }
+                console.log("Created database \"" + databaseName + "\".");
+                setUpTables(done);
+            }
+        );
+    });
+}
+
+function setUpSchemaCommand() {
+    console.log("Setting up database...");
+    setUpDatabase(function() {
+        console.log("Finished setting up database \"" + databaseName + "\".");
+        exitCleanly();
+    });
+}
+
+function verifyTableField(table, field, done) {
+    // TODO: Implement.
+    
+    done();
+}
+
+function verifyTableFields(table, done) {
+    // TODO: Implement.
+    
+    done();
+}
+
+function verifyTable(table, done) {
+    // TODO: Implement.
+    
+    done();
+}
+
+function verifyTables(done) {
+    var index = 0;
+    function verifyNextTable() {
+        if (index >= schemaConfig.tables.length) {
+            done();
+            return;
+        }
+        var tempTable = schemaConfig.tables[index];
+        index += 1;
+        tableExists(tempTable, function(exists) {
+            if (!exists) {
+                console.log("Table \"" + tempTable.name + "\" is missing.");
+                verifyNextTable();
+                return;
+            }
+            console.log("Table \"" + tempTable.name + "\" exists.");
+            verifyTable(verifyNextTable);
+        });
+    }
+    verifyNextTable();
+}
+
+function verifyDatabase(done) {
+    databaseExists(function(exists) {
+        if (!exists) {
+            console.log("Database \"" + databaseName + "\" is missing.");
+            done();
+            return;
+        }
+        console.log("Database \"" + databaseName + "\" exists.");
+        verifyTables(done);
+    });
+}
+
+function verifySchemaCommand() {
+    console.log("Verifying database...");
+    verifyDatabase(function() {
+        console.log("Finished verifying database.");
+        exitCleanly();
+    });
+}
+
+function destroyDatabase() {
     console.log("Destroying database...");
     // TODO: Implement.
     
@@ -38,9 +191,9 @@ function destroySchema() {
 
 function destroySchemaCommand() {
     confirm(
-        "Are you sure you want to destroy the database \"" + databaseConfig.databaseName + "\"?",
+        "Are you sure you want to destroy the database \"" + databaseName + "\"?",
         function () {
-            destroySchema();
+            destroyDatabase();
         },
         function () {
             console.log("Database NOT destroyed.");
@@ -58,12 +211,16 @@ function processCli() {
         setUpSchemaCommand();
     } else if (command == "destroy") {
         destroySchemaCommand();
+    } else if (command == "verify") {
+        verifySchemaCommand();
     } else {
         printUsageAndExit();
     }
 }
 
 var databaseConfig = JSON.parse(fs.readFileSync("./databaseConfig.json", "utf8"));
+var schemaConfig = JSON.parse(fs.readFileSync("./schemaConfig.json", "utf8"));
+var databaseName = databaseConfig.databaseName
 
 var args = parseArgs(process.argv.slice(2));
 
@@ -73,7 +230,7 @@ if (args["_"].length <= 0) {
 
 console.log("Connecting to MySQL...");
 
-var connection = mysql.createConnection({
+connection = mysql.createConnection({
     host: databaseConfig.host,
     user: databaseConfig.username,
     password: databaseConfig.password
@@ -81,10 +238,8 @@ var connection = mysql.createConnection({
 
 connection.connect(function(error) {
     if (error) {
-        console.log("Could not connect to MySQL.");
-        console.log(error.code);
-        console.log(error.sqlMessage);
-        process.exit(1);
+        reportSqlError(error);
+        exitCleanly();
         return;
     }
     console.log("Connected.");
